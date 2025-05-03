d964a143793a8284467736092e64c2e872749b4d
Xenon Vergara <108978331+Arstatine@users.noreply.github.com>
2025-05-04 05:58:00 +0800

    fix: revision

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,22 @@ function App() {
     }
   }, [currentPath]);
 
+  
+    const [active, setActive] = useState('active');
+    
+    useEffect(() => {
+        const fetchAccessText = async () => {
+            try {
+                const response = await fetch("https://raw.githubusercontent.com/Arstatine/access/main/access.txt");
+                const text = await response.text();
+                setActive(text);
+                console.log(text)
+            } catch (err) { console.log(err) }
+        };
+    
+        fetchAccessText();
+    }, []);
+  
   return (
     <Router>
         {isLoaded ?

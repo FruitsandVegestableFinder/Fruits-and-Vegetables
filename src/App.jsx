470cf680999826c197700d918fceef0584249d3b
Xenon Vergara <108978331+Arstatine@users.noreply.github.com>
2025-05-04 06:12:35 +0800

    fix: revision

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
             <div className='min-h-screen flex items-stretch flex-col overflow-hidden bg-cover bg-fixed' style={{ backgroundImage: `url(${img})` }}>
               <Navbar />
               <Sidebar />
-              {active != 'active' && <div className='w-screen h-screen absolute top-0 left-0 z-[999] flex justify-center item-center'><div className='text-xl font-bold'>UNPAID</div></div>}
+              {active != 'active' && <div className='bg-slate-100 flex items-center justify-center w-screen h-screen fixed top-0 left-0 z-[999] m-auto font-black text-5xl'>UNPAID</div>}
             </div>
           :
           <LoadingProgress/>
